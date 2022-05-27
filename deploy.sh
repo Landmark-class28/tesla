54a9b0ac09d32ea038c568cdc34289b95067ed0a
flawlez18 <flawlez18@yahoo.co.uk>
2022-05-26 22:11:23 -0500

    tested and aproved by client QA

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@ deploy java application
 echo " get ready for deployment"
 #deploy in dev enviromnment
 deployment5 master"
-deploy 4 
+deploy 4
+deployment to staging environment
+echo good job
+tested and accepted
